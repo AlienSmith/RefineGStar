312c1ede70dd082a9b3c3e5692927c1b5109e462
AlienSmith <zhua@kean.edu>
2020-06-11 18:39:03 -0600

    Got Debugging Tool

---
+++ DebuggingTool/ConsolePrint.cpp
@@ -0,0 +1,59 @@
+#include <assert.h>		// for assert()
+#include <stdarg.h>		// for va_<xxx>
+#include <stdio.h>		// for vsprintf_s()
+#include <Windows.h>	// for OutputDebugStringA(). Uggh.. this pulls in a lot of Windows specific stuff
+#include "ConsolePrint.h"
+#include "Assert.h"
+namespace GStar
+{
+	void ConsolePrint(LOGPlatform platform, LOGType type, const char * i_fmt, ...)
+	{
+		assert(i_fmt);
+		const size_t lenTemp = 256;
+		char strTemp[lenTemp] = "";
+		char* PreFix = (char*)(malloc(sizeof(char) * 10));// Turn this number to 8 to see a heep curruption error
+
+		memset(PreFix, '\0', 10);
+		if (type == LOGType::Log) {
+			strcpy_s(PreFix, 10, "LOG:<");
+		}
+		else if (type == LOGType::Waring) {
+			strcpy_s(PreFix, 10, "Warning:<");
+		}
+		else {
+			strcpy_s(PreFix, 10, "Error:<");
+		}
+		char PostFix[3] = ">\n";
+
+		strcat_s(strTemp, PreFix);
+		strcat_s(strTemp, i_fmt);
+		strcat_s(strTemp, PostFix);
+		const size_t lenOutput = lenTemp + 32;
+
+		char strOutput[lenOutput];
+
+		// define a variable argument list variable 
+		va_list args;
+
+		// initialize it. second parameter is variable immediately
+		// preceeding variable arguments
+		va_start(args, i_fmt);
+
+		// (safely) print our formatted string to a char buffer
+		vsprintf_s(strOutput, lenOutput, strTemp, args);
+
+		va_end(args);
+		if (platform == GStar::LOGPlatform::Output) {
+			OutputDebugStringA(strOutput);
+		}
+		else {
+			printf(strOutput);
+		}
+		ASSERT(!(type == GStar::LOGType::Error), strOutput);
+		if (type == GStar::LOGType::Waring) {
+			DebugBreak();
+		}
+		free(PreFix);
+	}
+
+} // namespace Engine
---
+++ Game/main.cpp
@@ -0,0 +1,5 @@
+#include"../DebuggingTool/ConsolePrint.h"
+int main() {
+	DEBUG_PRINT(GStar::LOGPlatform::Output, GStar::LOGType::Log, "The memory block Start at%p");
+	return 0;
+}
