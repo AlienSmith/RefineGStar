7f63231594b08f9414dcb852ac2fb52fe67ae6d5
AlienSmith <zhua@kean.edu>
2020-06-17 02:17:22 -0600

    Release version do not put initial fills or paddings

--- HeapManager/HeapManager.cpp
+++ HeapManager/HeapManager.cpp
@@ -4,63 +4,6 @@
 #include "../DebuggingTool/ConsolePrint.h"
 #include <stdio.h>
 #include <stdlib.h>
-//TODO make a effecient version for release
-//void* HeapManager::GeneralHeap = nullptr;
-//#if defined(_DEBUGACTIVITE)
-//HeapManager::HeapManager(size_t HeapSize, unsigned int numDescriptors, void * _pHeapMemeoy)
-//{
-//	this->_sizeHeap = HeapSize;
-//	this->_numDescriptors = numDescriptors;
-//	this->_pHeapMemory = _pHeapMemeoy;
-//	//memset(_pHeapMemeoy, '\0', HeapSize);
-//	DEBUG_PRINT(GStar::LOGPlatform::Output, GStar::LOGType::Log, "The memory block Start at%p", _pHeapMemeoy);
-//	_current = _pHeapMemeoy;
-//	INFOBLCOK* infoblock = reinterpret_cast<INFOBLCOK*>(_current);
-//#if defined(ENVIRONMENT64)
-//	memset(&(infoblock->start), HeapManager::fillguard, 8);
-//	memset(&(infoblock->end), HeapManager::fillguard, 8);
-//#elif defined(ENVIRONMENT32)
-//	memset(&(infoblock->start), HeapManager::fillguard, 4);
-//	memset(&(infoblock->end), HeapManager::fillguard, 4);
-//#endif
-//	infoblock->isusing = HeapManager::infoisnotusing;
-//	infoblock->size = HeapSize - sizeof(INFOBLCOK) - guardsize - 1; // start with info end with guards
-//
-//	_current = _movePointerForward(_current, sizeof(INFOBLCOK));
-//	memset(_current, HeapManager::fillinitialfilled, infoblock->size);
-//	_current = _movePointerForward(_current, infoblock->size);
-//	memset(_current, HeapManager::fillguard, guardsize);
-//	_current = _movePointerForward(_current, guardsize);
-//	char* end = reinterpret_cast<char*>(_current);
-//	*end = HeapManager::infoend;
-//	DEBUG_PRINT(GStar::LOGPlatform::Output, GStar::LOGType::Log, "%p", _current);
-//	DEBUG_PRINT(GStar::LOGPlatform::Output, GStar::LOGType::Log, "my info block have size %i", sizeof(INFOBLCOK));
-//	DEBUG_PRINT(GStar::LOGPlatform::Output, GStar::LOGType::Log, "size_t have size %i", sizeof(size_t));
-//	DEBUG_PRINT(GStar::LOGPlatform::Output, GStar::LOGType::Log, "char have size %i", sizeof(char));
-//	DEBUG_PRINT(GStar::LOGPlatform::Output, GStar::LOGType::Log, "start is %s", infoblock->start);
-//}
-//#else
-//HeapManager::HeapManager(size_t HeapSize, unsigned int numDescriptors, void * _pHeapMemeoy)
-//{
-//	this->_sizeHeap = HeapSize;
-//	this->_numDescriptors = numDescriptors;
-//	this->_pHeapMemory = _pHeapMemeoy;
-//	//memset(_pHeapMemeoy, '\0', HeapSize);
-//	DEBUG_PRINT(GStar::LOGPlatform::Output, GStar::LOGType::Log, "The memory block Start at%p", _pHeapMemeoy);
-//	_current = _pHeapMemeoy;
-//	INFOBLCOK* infoblock = reinterpret_cast<INFOBLCOK*>(_current);
-//	infoblock->isusing = HeapManager::infoisnotusing;
-//	infoblock->size = HeapSize - sizeof(INFOBLCOK) - 1; //no guard 1 byte for end
-//
-//	_current = _movePointerForward(_current, sizeof(INFOBLCOK));
-//	memset(_current, HeapManager::fillinitialfilled, infoblock->size);
-//	_current = _movePointerForward(_current, infoblock->size);
-//	memset(_current, HeapManager::fillguard, guardsize);
-//	_current = _movePointerForward(_current, guardsize);
-//	char* end = reinterpret_cast<char*>(_current); // it could be uint_8 since only one byte for this info
-//	*end = HeapManager::infoend;
-//}
-//#endif
 #if defined(_DEBUGACTIVITE)
 void HeapManager::InitializeWith(size_t HeapSize, unsigned int numDescriptors, void * _pHeapMemeoy)
 {
@@ -354,8 +297,8 @@ bool HeapManager::_TryCut(size_t size, unsigned int alignment)
 		//To heigher address
 		//InfoBlock + User data + Padding
 		void* temppointer = _movePointerForward(_current, size);
-		memset(temppointer, HeapManager::fillpadding, padding);
-		memset(_current, HeapManager::fillinitialfilled, size);
+		//memset(temppointer, HeapManager::fillpadding, padding);
+		//memset(_current, HeapManager::fillinitialfilled, size);
 		_current = _movePointerBackward(_current, sizeof(INFOBLCOK));
 		//this function will move _current forward to user's location
 		_addinfoblock(size+padding); // realsize
